455ff51f3ff3a10d2301857e42d1f9860c5702a3
Ibrahim <alnajjar.ibrahim@gmail.com>
2025-05-16 08:46:46 +0200

    seperate the get to new folder called router

--- main.py
+++ main.py
@@ -1,60 +1,7 @@
-from fastapi import FastAPI , status , Response
-from enum import Enum
-from typing import Optional
+from fastapi import FastAPI 
 
 app = FastAPI()
 @app.get("/")
 def simple():
     return {"message Hello api"}
-
-@app.get("/tasks")
-def get_another_Method():
-    return {"Hello with tasks "}
-@app.get("/tasks/todo")
-def get_another_return():
-    return {
-        "message " : "I started to get what i am doing i am start to now fast today "
-        }
-
-# @app.post("/")
-# def get_method_Post():
-#     return {"i am post it and i want to see how is it "}
-@app.get("/blog/all")
-def GetAll():
-    return {"message" :" i am modee and i want vahsa  "}
-
-class blogType(str , Enum):
-        short = "short"
-        long = "long"
-        howto = "howto"
-        mybrother ="ismail"
-
-@app.get("/taskss/{type}" 
-         
-          )
-def UseGetEnum(type :blogType  ):
-     return {"message":f"Hello Enum:{type}"}
-
-@app.get("/get")
-def putPerameter(pages = 1, numberPages: Optional[int] = None):
-     return {"message" : f"the total pages is {pages} and you are in number {numberPages}"}
-
-
-@app.get("/try/{id}/comment/{commit_Id}")
-def get_all_blog(id: int , commit_Id : int , valid : bool = True , username :Optional[str] = None):
-     return {"message":f"id : {id} , commit_Id : {commit_Id} , valid : {valid} , username : {username}"}
-
-
-@app.get("/ismail/{id}" , status_code= status.HTTP_102_PROCESSING , 
-           tags= ['isn' ,'YTy'] ,
-          summary="this is to try summary"
-          , description="i want to try also the description of the parameters" )
-def GetIsmailId(id : int , response : Response):
-    if id > 5 : 
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {f"Id  {id} is not found"}
-    else: 
-         response.status_code = status.HTTP_200_OK
-         return {f"id {id} is found"}
-
 
